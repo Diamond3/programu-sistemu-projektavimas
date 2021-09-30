62991821065520fe5cd1c1bd443bd118482af987
Deimantas Eidukevicius <Diamond322222@gmail.com>
2021-09-30 22:05:21 +0300

    Update EmailValidatorTest.java
    Changes in tests

--- test/com/alemal/validation/EmailValidatorTest.java
+++ test/com/alemal/validation/EmailValidatorTest.java
@@ -3,8 +3,7 @@ package com.alemal.validation;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class EmailValidatorTest {
 
@@ -40,49 +39,112 @@ class EmailValidatorTest {
      * Test cases for VALID Email addresses
      * */
     @Test
-    void test_valid_usernameSimple() {
-        assertTrue(emailValidator.validate("simple@example.com"));
-        assertTrue(emailValidator.validate("x@example.com"));
+    void test_valid_localPathSimple() {
+        assertAll(
+                () -> assertTrue(emailValidator.validate("simple@example.com")),
+                () -> assertTrue(emailValidator.validate("x@example.com"))
+        );
     }
 
     @Test
-    void test_valid_usernameWithDot() {
+    void test_valid_localPathWithDot() {
         assertTrue(emailValidator.validate("very.common@example.com"));
     }
 
     @Test
-    void test_valid_usernameWithPlusSymbol() {
+    void test_valid_localPathWithPlusSymbol() {
         assertTrue(emailValidator.validate("user.name+tag+sorting@example.com"));
     }
 
     @Test
-    void test_valid_usernameWithHyphen() {
-        assertTrue(emailValidator.validate("fully-qualified-domain@example.com"));
-        assertTrue(emailValidator.validate("user-@example.org"));
+    void test_valid_localPathWithHyphen() {
+        assertAll(
+                () -> assertTrue(emailValidator.validate("fully-qualified-domain@example.com")),
+                () -> assertTrue(emailValidator.validate("user-@example.org"))
+                );
     }
 
     @Test
-    void test_valid_usernameWithSpecialChar() {
-        assertTrue(emailValidator.validate("mailhost!username@example.org"));
-        assertTrue(emailValidator.validate("user%example.com@example.org"));
-        assertTrue(emailValidator.validate("test/test@test.com"));
+    void test_valid_localPathWithSpecialChar() {
+        assertAll(
+                () -> assertTrue(emailValidator.validate("mailhost!username@example.org")),
+                () -> assertTrue(emailValidator.validate("user%example.com@example.org")),
+                () -> assertTrue(emailValidator.validate("test/test@test.com"))
+                );
+    }
+
+
+    @Test
+    void test_invalid_localPath() {
+        assertFalse(emailValidator.validate("Joe Smith <email@example.com>"));
     }
 
     @Test
-    void test_valid_usernameWithQuotationMarks_doubleDot() {
-        assertTrue(emailValidator.validate("\"john..doe\"@example.org"));
+    void test_invalid_localPathEmpty() {
+        assertFalse(emailValidator.validate("@example.com"));
     }
 
     @Test
-    void test_valid_usernameWithQuotationMarks_space() {
-        assertTrue(emailValidator.validate("\" \"@example.org"));
-        assertTrue(emailValidator.validate("much.\"more unusual\"@example.com"));
+    void test_invalid_localPathFirstCharIsDot() {
+        assertFalse(emailValidator.validate(".email@example.com"));
     }
 
     @Test
-    void test_valid_usernameWithQuotationMarks_secondEta() {
-        assertTrue(emailValidator.validate("very.unusual.\"@\".unusual.com@example.com"));
+    void test_invalid_localPathLastCharIsDot() {
+        assertFalse(emailValidator.validate("email.@example.com"));
+
     }
+
+    @Test
+    void test_invalid_localPathLength() {
+        assertFalse(emailValidator.validate("1234567890123456789012345678901234567890123456789012345678901234+x@example.com"));
+    }
+
+    @Test
+    void test_invalid_localPathQuotationMark() {
+        assertFalse(emailValidator.validate("simple\"email@example.com"));
+    }
+
+    @Test
+    void test_invalid_localPathWithSpecialChar_parenthesis() {
+        assertFalse(emailValidator.validate("simple(text)email@example.com"));
+    }
+
+    @Test
+    void test_invalid_localPathWithSpecialChar_comma() {
+        assertFalse(emailValidator.validate("simple,email@example.com"));
+    }
+
+    @Test
+    void test_invalid_localPathWithSpecialChar_space() {
+        assertFalse(emailValidator.validate("simple email@example.com"));
+    }
+
+    @Test
+    void test_invalid_localPathWithSpecialChar_colon() {
+        assertFalse(emailValidator.validate("simple:email@example.com"));
+    }
+
+    @Test
+    void test_invalid_localPathWithSpecialChar_backslash() {
+        assertFalse(emailValidator.validate("simple\\email@example.com"));
+    }
+
+//    @Test
+//    void test_valid_localPathWithQuotationMarks_doubleDot() {
+//        assertTrue(emailValidator.validate("\"john..doe\"@example.org"));
+//    }
+
+//    @Test
+//    void test_valid_localPathWithQuotationMarks_space() {
+//        assertTrue(emailValidator.validate("\" \"@example.org"));
+//        assertTrue(emailValidator.validate("much.\"more unusual\"@example.com"));
+//    }
+
+//    @Test
+//    void test_valid_localPathWithQuotationMarks_secondEta() {
+//        assertTrue(emailValidator.validate("very.unusual.\"@\".unusual.com@example.com"));
+//    }
 
     @Test
     void test_valid_domainWithHyphen() {
@@ -91,104 +153,20 @@ class EmailValidatorTest {
 
     @Test
     void test_valid_domainWithSubdomain() {
-        assertTrue(emailValidator.validate("example@s.example"));
-        assertTrue(emailValidator.validate("email@subdomain.example.com"));
+        assertAll(
+                () -> assertTrue(emailValidator.validate("example@s.example.lt")),
+                () -> assertTrue(emailValidator.validate("email@subdomain.example.com"))
+                );
     }
 
     @Test
     void test_valid_domainTLD() {
-        assertTrue(emailValidator.validate("email@example.name"));
-        assertTrue(emailValidator.validate("email@example.museum"));
-        assertTrue(emailValidator.validate("email@example.co.jp"));
-        assertTrue(emailValidator.validate("email@example.lt"));
-    }
-
-    @Test
-    void test_valid_domainIP() {
-        assertTrue(emailValidator.validate("email@[123.123.123.123]"));
-    }
-
-    /**
-     * Test cases for INVALID Email addresses
-     * */
-
-    @Test
-    void test_invalid_withoutEtaSymbol() {
-        assertFalse(emailValidator.validate("plainaddress"));
-        assertFalse(emailValidator.validate("email.example.com"));
-    }
-
-    @Test
-    void test_invalid_tooManyEtas() {
-        assertFalse(emailValidator.validate("#@email@d.com"));
-        assertFalse(emailValidator.validate("email@example@example.com"));
-    }
-
-    @Test
-    void test_invalid_doubleDot() {
-        assertFalse(emailValidator.validate("email@example..com"));
-        assertFalse(emailValidator.validate("Abc..123@example.com"));
-    }
-
-    @Test
-    void test_invalid_username() {
-        assertFalse(emailValidator.validate("Joe Smith <email@example.com>"));
-    }
-
-    @Test
-    void test_invalid_usernameEmpty() {
-        assertFalse(emailValidator.validate("@example.com"));
-    }
-
-    @Test
-    void test_invalid_usernameFirstCharIsDot() {
-        assertFalse(emailValidator.validate(".email@example.com"));
-    }
-
-    @Test
-    void test_invalid_usernameLastCharIsDot() {
-        assertFalse(emailValidator.validate("email.@example.com"));
-
-    }
-
-    @Test
-    void test_invalid_usernameChars() {
-        assertFalse(emailValidator.validate("あいうえお@example.com"));
-    }
-
-    @Test
-    void test_invalid_usernameLength() {
-        assertFalse(emailValidator.validate("1234567890123456789012345678901234567890123456789012345678901234+x@example.com"));
-    }
-
-    @Test
-    void test_invalid_usernameQuotationMark() {
-        assertFalse(emailValidator.validate("simple\"email@example.com"));
-    }
-
-    @Test
-    void test_invalid_usernameWithSpecialChar_parenthesis() {
-        assertFalse(emailValidator.validate("simple(text)email@example.com"));
-    }
-
-    @Test
-    void test_invalid_usernameWithSpecialChar_comma() {
-        assertFalse(emailValidator.validate("simple,email@example.com"));
-    }
-
-    @Test
-    void test_invalid_usernameWithSpecialChar_space() {
-        assertFalse(emailValidator.validate("simple email@example.com"));
-    }
-
-    @Test
-    void test_invalid_usernameWithSpecialChar_colon() {
-        assertFalse(emailValidator.validate("simple:email@example.com"));
-    }
-
-    @Test
-    void test_invalid_usernameWithSpecialChar_backslash() {
-        assertFalse(emailValidator.validate("simple\\email@example.com"));
+        assertAll(
+                () -> assertTrue(emailValidator.validate("email@example.name")),
+                () -> assertTrue(emailValidator.validate("email@example.museum")),
+                () -> assertTrue(emailValidator.validate("email@example.co.jp")),
+                () -> assertTrue(emailValidator.validate("email@example.lt"))
+                );
     }
 
     @Test
@@ -214,5 +192,34 @@ class EmailValidatorTest {
     @Test
     void test_invalid_domainTLD() {
         assertFalse(emailValidator.validate("email@111.222.333.44444"));
+    }
+
+//    @Test
+//    void test_valid_domainIP() {
+//        assertTrue(emailValidator.validate("email@[123.123.123.123]"));
+//    }
+
+    @Test
+    void test_invalid_withoutEtaSymbol() {
+        assertAll(
+                () -> assertFalse(emailValidator.validate("plainaddress")),
+                () -> assertFalse(emailValidator.validate("email.example.com"))
+                );
+    }
+
+    @Test
+    void test_invalid_tooManyEtas() {
+        assertAll(
+                () -> assertFalse(emailValidator.validate("#@email@d.com")),
+                () -> assertFalse(emailValidator.validate("email@example@example.com"))
+                );
+    }
+
+    @Test
+    void test_invalid_doubleDot() {
+        assertAll(
+                () -> assertFalse(emailValidator.validate("email@example..com")),
+                () -> assertFalse(emailValidator.validate("Abc..123@example.com"))
+        );
     }
 }
